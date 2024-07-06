1c72a3ee442ccbea37224f595cd6420633911a3c
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-06 17:04:33 -0400

    everything-functional

--- js/script.js
+++ js/script.js
@@ -3,20 +3,21 @@ const apiKey = '820214c8a31808ffafdb376998c9d618';
 const secondApiKey = '996b5fa3416b1804f4d4cda0e5653d5d';
 const cityArray = [];
 //function for outputting the weather
-function outputWeather() {
-  event.preventDefault();
-  const cityInput = document.getElementById('city-input');
+function outputWeather(cityIn) {
+  //const cityInput = document.getElementById('city-input');
+  console.log("This is the city = ", cityIn);
 
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=imperial`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&units=imperial`;
   $.get(weatherUrl)
     .then(function (data) {
       currentDate = new Date();
+      console.log(data.dt);
       //idk why but data.weather.icon is undef... :/
       const weatherResults = 
       `<div>
         <h2>${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}</h2>
         <br>
-        <img src=" https://openweathermap.org/img/wn/${data.weather.icon}2x.png" alt = 'weather icon image'>
+        <img src= "https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon image">
         <br>
         Temp: ${data.main.temp} &deg
         <br>
@@ -37,18 +38,18 @@ function outputForecast(cityLatIn, cityLonIn) {
   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLatIn}&lon=${cityLonIn}&appid=${secondApiKey}&units=imperial`;
   $.get(forecastUrl)
     .then(function (data) {
-      const $forecastOutput = $('.forecast-output');
+      var $forecastOutput = $('.forecast-output');
       // Filter out from the 40 weather objects you receive, the 5 noon time weather objects for the next 5 days
       const filtered = data.list.filter(function (weatherObj) {
         if (weatherObj.dt_txt.includes('12')) return true;
       });
-    
-      forecastResults = '';
+      //clearing out the previous result
+      $forecastOutput.empty();
       filtered.forEach(function (weatherObj) {
         forecastDate = new Date(weatherObj.dt_txt);
         $forecastOutput.append(
           `<div>
-            <h2>${forecastDate.getMonth()+1}/${forecastDate.getDate()+1}/${forecastDate.getFullYear()}</h2>
+            <h2>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h2>
             <br>
             <img src="https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png" alt="weather icon image">
             <br>
@@ -68,28 +69,30 @@ function outputCities() {
   cityArray.push(cityInput.value);
   localStorage.setItem("data", JSON.stringify(cityArray));
   const $cityOutput = $('.storage-output');
+  $cityOutput.empty();
   
   //could also do forEach here, just JS forEach's are weird
   for(var i = 0; i < JSON.parse(localStorage.getItem("data")).length; i++) {
-    $cityOutput.append(
-      `<div>
-      ${JSON.parse(localStorage.getItem("data"))[i]}
-      <br>
-      </div>`
-    )
+    let newButton = $("<div>");
+    newButton.text(JSON.parse(localStorage.getItem("data"))[i])
+    newButton.click(historySearch)
+    $cityOutput.append(newButton)
   }
   localStorage.clear();
-  clearCities();
+
 }
 
-function clearCities() {
-  var $cities = $('.storage-output');
-  $cities = "";
+function historySearch(event){
+outputWeather(event.target.innerHTML);
 }
 
-weatherBtn.addEventListener('click', outputWeather);
-weatherBtn.addEventListener('click', outputCities);
+function startWeather(event){
+  event.preventDefault();
+  const cityInput = document.getElementById('city-input');
+  outputWeather(cityInput.value)
+  outputCities();
+}
+
+weatherBtn.addEventListener("submit", startWeather);
 
 
-//TODO:
-//clear the local storage, and make them links
